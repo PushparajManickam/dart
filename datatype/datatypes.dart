ae2963885d6cabae5b1ad28c9a3cf33c02805bb3
Pushparaj Manickam <47779182+PushparajManickam@users.noreply.github.com>
2022-12-13 17:29:31 +0530

    Create datatypes.dart

---
+++ datatype/datatypes.dart
@@ -0,0 +1,9 @@
+void main(){
+  /*String Datatype*/
+  //With string datatype
+  String stringValue = "Hello flutter developer";
+  print(stringValue);
+  //Without string datatype
+  var varValue = "Welcome to dart language";
+  print(varValue);
+}
